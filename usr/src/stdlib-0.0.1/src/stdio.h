8260a4e8eacbcd9926e35837748a7b0ec9a0bc61
Patrick Günthard <patrick.guenthard@adnovum.ch>
2017-11-23 14:57:35 +0100

    stdlib: updated stdio c definition

--- usr/src/stdlib-0.0.1/src/stdio.h
+++ usr/src/stdlib-0.0.1/src/stdio.h
@@ -20,6 +20,23 @@
 #ifndef STDIO_H
 #define STDIO_H
 
+typedef struct _iobuf
+{
+    char*   _ptr;
+    int _cnt;
+    char*   _base;
+    int _flag;
+    int _file;
+    int _charbuf;
+    int _bufsiz;
+    char*   _tmpfname;
+} FILE;
+
+
+
+
+//typedef long unsigned int size_t;
+
 int printf(const char *format, ...);
 
 int fprintf(FILE *stream, const char *format, ...);
